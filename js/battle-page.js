90bccc41da47be27432e22b003f6e1561dc8b3b2
Diana Dukhovskaya <k.dianavv@gmail.com>
2025-08-24 16:50:50 +0100

    feat: add all items to localStorage for fight

--- js/battle-page.js
+++ js/battle-page.js
@@ -1,5 +1,68 @@
 const chosenCharImg = document.getElementById('choosen__char__img');
 
+const attackRadios = document.querySelectorAll('input[name="attack"]');
+const defenceCheckboxes = document.querySelectorAll('input[name="defence"]');
+const attackBtn = document.querySelector('.attack__btn');
+const charHealthEl = document.querySelector(".char__health");
+const enemyHealthEl = document.querySelector(".enemy__health");
+const charHealthImg = document.querySelector('.char__health__img');
+const enemyHealthImg = document.querySelector('.enemy__health__img');
+
+const enemies = [
+  { name: "Thornel", src: "assets/png/enemies/1.png" },
+  { name: "Veyra", src: "assets/png/enemies/2.png" },
+  { name: "Koril", src: "assets/png/enemies/3.png" },
+  { name: "Sylth", src: "assets/png/enemies/4.png" }
+];
+
+const bodyParts = ["Head", "Neck", "Body", "Belly", "Legs"];
+
+let enemyName = getRandomItem(enemies);
+const enemyNameOutput = document.querySelector('.enemy__name');
+const chosenEnemyImg = document.getElementById('random__enemy__img');
+let enemyAttackZone = getRandomItem(bodyParts);
+let enemyDefenceZones = getRandomItems(bodyParts, 2);
+const savedEnemy = localStorage.getItem('enemy');
+
+const logsBox = document.querySelector('.logs__wrapper');
+const savedLogs = localStorage.getItem("logs");
+
+let charHealth = 140;
+let enemyHealth = 140;
+
+const healthImages = [
+  { health: 140, src: "assets/png/health/7.png" },
+  { health: 120, src: "assets/png/health/6.png" },
+  { health: 100, src: "assets/png/health/5.png" },
+  { health: 80, src: "assets/png/health/4.png" },
+  { health: 60, src: "assets/png/health/3.png" },
+  { health: 40, src: "assets/png/health/2.png" },
+  { health: 20, src: "assets/png/health/1.png" },
+  { health: 0, src: "assets/png/health/0.png" }
+];
+
+attackBtn.disabled = true;
+
+// check saved fight start ----------------------
+if (savedLogs) {
+  logsBox.innerHTML = savedLogs;
+};
+if (savedEnemy) {
+  enemyName = JSON.parse(savedEnemy);
+}
+const savedCharHealth = localStorage.getItem("charHealth");
+const savedEnemyHealth = localStorage.getItem("enemyHealth");
+if (savedCharHealth && savedEnemyHealth) {
+  charHealth = parseInt(savedCharHealth, 10);
+  enemyHealth = parseInt(savedEnemyHealth, 10);
+
+  charHealthEl.innerHTML = `${charHealth}/140`;
+  enemyHealthEl.innerHTML = `${enemyHealth}/140`;
+  charHealthImg.src = localStorage.getItem("charHealthImg");
+  enemyHealthImg.src = localStorage.getItem("enemyHealthImg");
+}
+// check saved fight end ----------------------
+
 if (chosenCharImg) {
   const savedSrc = localStorage.getItem('selectedCharacterSrc');
   const savedAlt = localStorage.getItem('selectedCharacterAlt');
@@ -17,15 +80,9 @@ if (characterName) {
 }
 
 
-// enemy box
-const enemies = [
-  { name: "Thornel", src: "assets/png/enemies/1.png" },
-  { name: "Veyra", src: "assets/png/enemies/2.png" },
-  { name: "Koril", src: "assets/png/enemies/3.png" },
-  { name: "Sylth", src: "assets/png/enemies/4.png" }
-];
 
-const bodyParts = ["Head", "Neck", "Body", "Belly", "Legs"];
+// enemy box
+
 
 function getRandomItem(array) {
   const index = Math.floor(Math.random() * array.length);
@@ -45,16 +102,12 @@ function getRandomItems(array, count) {
   return result;
 };
 
-const enemyName = getRandomItem(enemies);
-const enemyNameOutput = document.querySelector('.enemy__name');
-const chosenEnemyImg = document.getElementById('random__enemy__img');
-let enemyAttackZone = getRandomItem(bodyParts);
-let enemyDefenceZones = getRandomItems(bodyParts, 2);
-
+// --------------------------------------------------------------------------------
 if (enemyName) {
   enemyNameOutput.innerHTML = enemyName.name;
   chosenEnemyImg.src = enemyName.src;
 }
+
 
 function generateEnemyMove() {
   enemyAttackZone = getRandomItem(bodyParts);
@@ -62,26 +115,6 @@ function generateEnemyMove() {
 }
 // enemy box end
 
-const attackRadios = document.querySelectorAll('input[name="attack"]');
-const defenceCheckboxes = document.querySelectorAll('input[name="defence"]');
-const attackBtn = document.querySelector('.attack__btn');
-const charHealthEl = document.querySelector(".char__health");
-const enemyHealthEl = document.querySelector(".enemy__health");
-const charHealthImg = document.querySelector('.char__health__img');
-const enemyHealthImg = document.querySelector('.enemy__health__img');
-let charHealth = 140;
-let enemyHealth = 140;
-
-const healthImages = [
-  { health: 140, src: "assets/png/health/7.png" },
-  { health: 120, src: "assets/png/health/6.png" },
-  { health: 100, src: "assets/png/health/5.png" },
-  { health: 80, src: "assets/png/health/4.png" },
-  { health: 60, src: "assets/png/health/3.png" },
-  { health: 40, src: "assets/png/health/2.png" },
-  { health: 20, src: "assets/png/health/1.png" },
-  { health: 0, src: "assets/png/health/0.png" }
-];
 
 function getHealthImage(currentHealth) {
   for (let i = 0; i < healthImages.length; i++) {
@@ -92,7 +125,7 @@ function getHealthImage(currentHealth) {
   return healthImages[0].src;
 }
 
-attackBtn.disabled = true;
+
 
 function makeAttack() {
   const attackSelected = [...attackRadios].find(a => a.checked);
@@ -116,9 +149,15 @@ attackBtn.addEventListener('click', (e) => {
   checkFightEnd();
   charHealthImg.src = getHealthImage(charHealth);
   enemyHealthImg.src = getHealthImage(enemyHealth);
+  localStorage.setItem("logs", logsBox.innerHTML);
+  localStorage.setItem("enemy", JSON.stringify(enemyName));
+  localStorage.setItem("charHealth", charHealth);
+  localStorage.setItem("enemyHealth", enemyHealth);
+  localStorage.setItem("charHealthImg", charHealthImg.src);
+  localStorage.setItem("enemyHealthImg", enemyHealthImg.src);
 });
 
-const logsBox = document.querySelector('.logs__wrapper');
+
 
 function attackResult() {
   const charAttackSelected = [...attackRadios].find(a => a.checked)?.value;
@@ -188,11 +227,13 @@ function checkFightEnd() {
   } else {
     return;
   }
-  localStorage.removeItem("charHealth");
-  localStorage.removeItem("enemyHealth");
 }
 
 function resetFight() {
+  localStorage.removeItem("charHealth");
+  localStorage.removeItem("enemyHealth");
+  localStorage.removeItem("logs");
+  localStorage.removeItem("enemy");
   charHealth = 140;
   enemyHealth = 140;
   charHealthEl.innerHTML = `${charHealth}/140`;
@@ -200,6 +241,12 @@ function resetFight() {
   logsBox.innerHTML = '';
   charHealthImg.src = getHealthImage(charHealth);
   enemyHealthImg.src = getHealthImage(enemyHealth);
+
+  enemyName = getRandomItem(enemies);
+  enemyNameOutput.innerHTML = enemyName.name;
+  chosenEnemyImg.src = enemyName.src;
+  localStorage.setItem("enemy", JSON.stringify(enemyName));
+
   [...attackRadios, ...defenceCheckboxes].forEach(i => i.checked = false);
   attackBtn.disabled = true;
 }
